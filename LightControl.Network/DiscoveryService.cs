e12456d3b86f9cdd48c6ff800d1c49ed63f40e27
Jakub Potocki <kubac65@gmail.com>
2019-02-13 17:50:19 +0000

    Added DiscoveryService

---
+++ LightControl.Network/DiscoveryService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LightControl.Network
+{
+    public delegate void DeviceDiscovered(object sender, DeviceDiscoveredEventArgs eventArgs);
+
+    public class DeviceDiscoveredEventArgs : EventArgs
+    {
+        public IPAddress Address { get; set; }
+        public byte[] Mac { get; set; }
+    }
+
+    public class DiscoveryService : IDisposable
+    {
+        private static int Port = 54545;
+
+        private UdpClient _udp = new UdpClient();
+        private IPEndPoint _src = new IPEndPoint(IPAddress.Any, Port);
+
+        public event DeviceDiscovered DeviceDiscovered;
+
+        public DiscoveryService()
+        {
+            _udp.Client.Bind(_src);
+        }
+
+        public void BeginReceive()
+        {
+            _udp.BeginReceive(new AsyncCallback(ReceiveMessage), this);
+        }
+
+        public void Dispose()
+        {
+            _udp.Dispose();
+        }
+
+        private void ReceiveMessage(IAsyncResult ar)
+        {
+            byte[] receivedMessage = _udp.EndReceive(ar, ref _src);
+            DeviceDiscovered?.Invoke(this, new DeviceDiscoveredEventArgs()
+            {
+                Address = _src.Address,
+                Mac = receivedMessage
+            });
+            BeginReceive();
+        }
+    }
+}
--- LightControl/MainActivity.cs
+++ LightControl/MainActivity.cs
@@ -3,17 +3,52 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using LightControl.Network;
+using System.Text;
+using System.Collections.Generic;
+using Android.Content;
 
 namespace LightControl
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private DiscoveryService ds = new DiscoveryService();
+        private List<string> ips = new List<string>();
+
+        private ArrayAdapter<string> items;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            items = new ArrayAdapter<string>(this, Resource.Id.devices, ips);
+                       
+            ds.DeviceDiscovered += Ds_DeviceDiscovered;
+            ds.BeginReceive();
+        }
+
+        private void Ds_DeviceDiscovered(object sender, DeviceDiscoveredEventArgs eventArgs)
+        {
+            var devices = this.FindViewById<ListView>(Resource.Id.devices);
+            string receiveString = Encoding.ASCII.GetString(eventArgs.Mac);
+            if (ips.Contains(eventArgs.Address.ToString()))
+            {
+                return;
+            }
+
+            ips.Add(eventArgs.Address.ToString());
+            this.RunOnUiThread(() =>
+            {
+                items.Add(eventArgs.Address.ToString());
+                devices.Adapter = items;
+            });
+
+            //var label = new TextView(this);
+            //label.Text = eventArgs.Address.ToString();
+            //devices.AddView(label);
+
         }
     }
 }
