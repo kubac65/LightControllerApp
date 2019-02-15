c49435deff7fe139d319e86a46e9d46da1528dc5
Jakub Potocki <kubac65@gmail.com>
2019-02-15 00:15:17 +0000

    Added 'echo' functionality to test DeviceClient

--- LightControl.Network/DeviceClient.cs
+++ LightControl.Network/DeviceClient.cs
@@ -6,23 +6,46 @@
 namespace LightControl.Network
 {
     using System;
-    using System.IO;
     using System.Net;
     using System.Net.Sockets;
+    using System.Text;
 
-    internal class DeviceClient : IDisposable
+    /// <summary>
+    /// Client used for comunicating with control device.
+    /// </summary>
+    public class DeviceClient : IDisposable
     {
-        private TcpClient _client;
+        private readonly TcpClient _client;
+        private readonly NetworkStream _stream;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceClient"/> class.
+        /// </summary>
+        /// <param name="address">IP address of the control device.</param>
+        /// <param name="port">Listening port of the control device.</param>
         public DeviceClient(IPAddress address, int port)
         {
             _client = new TcpClient();
             _client.Connect(new IPEndPoint(address, port));
+            _stream = _client.GetStream();
         }
 
+        public string SendEcho(string msg)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(msg);
+            _stream.Write(data, 0, data.Length);
+
+            byte[] response = new byte[data.Length];
+            int bytesRead = _stream.Read(response, 0, data.Length);
+            string receivedMsg = Encoding.ASCII.GetString(response);
+            return receivedMsg;
+        }
+
+        /// <inheritdoc/>
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _stream.Close();
+            _client.Close();
         }
     }
 }
